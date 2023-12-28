65cd6a8cb5eb7e94670f56776fc10c3d30a49a75
ruman <rumancha12@gmail.com>
2023-12-28 21:51:27 +0545

    added config file

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from typing_extensions import Annotated
+from functools import lru_cache
+from .config import Settings
 
 app = FastAPI()
 
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+@app.get("/config/")
+def read_config(settings: Annotated[Settings, Depends(get_settings)]):
+    return {"DB_URL": settings.DB_URL, "Debug Mode": settings.DEBUG}
 @app.get("/")
 async def root():
     return {"message" : "Hello world"}
