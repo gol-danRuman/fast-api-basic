07b91eb0ffd99ed391d661231789bad3be3d40f2
ruman <rumancha12@gmail.com>
2023-12-28 23:18:12 +0545

    connected postgres db

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import find_dotenv, load_dotenv
+import os
 
 load_dotenv(find_dotenv(".env"))
 
@@ -10,7 +11,17 @@ class Settings(BaseSettings):
     items_per_user: int = 50
     DB_URL: str
     DEBUG: bool
+    PROJECT_NAME: str = "My Website"
+    PROJECT_VERSION: str = "1.0.0"
+
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)  # default postgres port is 5432
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
+    DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     model_config = SettingsConfigDict(case_sensitive=True)
+
 
 settings = Settings()
--- main.py
+++ main.py
@@ -1,33 +1,60 @@
-
 from fastapi import FastAPI
-from router import web_chat_router, nepse_router
-
+from router import web_chat_router
 
 from fastapi import FastAPI, Depends
 from typing_extensions import Annotated
 from functools import lru_cache
 from config import Settings
-
-app = FastAPI()
-app.include_router(web_chat_router.router)
-app.include_router(nepse_router.router)
-
+from db.session import engine
+from db.base_class import Base
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @lru_cache
 def get_settings():
     return Settings()
 
+
 settings = get_settings()
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    origins = [
+        "http://localhost",
+        "http://localhost:3000",
+    ]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    create_tables()
+    return app
+
+
+app = start_application()
+app.include_router(web_chat_router.router)
+# app.include_router(nepse_router.router)
+
+
+
 
 
 @app.get("/config/")
 def read_config(settings: Annotated[Settings, Depends(get_settings)]):
     return {"DB_URL": settings.DB_URL, "Debug Mode": settings.DEBUG}
 
+
 @app.get("/")
 async def root():
-    return {"message" : "Hello world", "DB_URL": settings.DB_URL, "Debug Mode": settings.DEBUG}
+    return {"message": "Hello world", "DB_URL": settings.DB_URL, "Debug Mode": settings.DEBUG}
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id):
