4455f3372191ef5dad5f8e784430de99852c1e65
ruman <rumancha12@gmail.com>
2023-12-29 19:21:16 +0545

    added env for db

--- config.py
+++ config.py
@@ -1,18 +1,93 @@
+from dataclasses import Field
+from typing import Optional, List
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import find_dotenv, load_dotenv
 import os
+from pydantic import BaseModel
 
-load_dotenv(find_dotenv(".env"))
+# load_dotenv(find_dotenv(".env"))
 
 
-class Settings(BaseSettings):
-    app_name: str = "My Website"
-    admin_email: str = "rumancha12@gmail.com"
-    items_per_user: int = 50
-    DB_URL: str
-    DEBUG: bool
+# class Settings(BaseSettings):
+#     app_name: str = "My Website"
+#     admin_email: str = "rumancha12@gmail.com"
+#     items_per_user: int = 50
+#     DB_URL: str
+#     DEBUG: bool
+#     PROJECT_NAME: str = "My Website"
+#     PROJECT_VERSION: str = "1.0.0"
+#
+#     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+#     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+#     POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+#     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)  # default postgres port is 5432
+#     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
+#     DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}/{POSTGRES_DB}"
+#
+#     model_config = SettingsConfigDict(case_sensitive=True)
+#
+#
+# settings = Settings()
+
+class AppConfig(BaseModel):
+    DESCRIPTION: str = 'API'
+    VERSION: float = 1.0
+    PORT: int = 8000
+
+
+class GlobalConfig(BaseSettings):
+    APP_CONFIG: AppConfig = AppConfig()
+
+
+    # model_config = SettingsConfigDict(case_sensitive=True, env_file=".env")
+
+class DevConfig(GlobalConfig):
+    TITLE: str = 'DEV'
+
+    HOST: str = "localhost"
+
+    DEBUG: bool = True
+
+    BASE_PATH: str = os.getcwd()
+
     PROJECT_NAME: str = "My Website"
     PROJECT_VERSION: str = "1.0.0"
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+
+    origins: str = "*"
+
+    openapi_url: Optional[str] = '/api/openapi'
+
+    docs_url: Optional[str] = '/docs'
+
+    redoc_url: Optional[str] = '/redoc'
+
+    proxy_headers: bool = False
+
+    forwarded_allow_ips: List[str] = ["*"]
+
+    WHITELISTED_IPS: List[str] = ["*"]
+
+
+    model_config = SettingsConfigDict()
+
+
+class ProdConfig(GlobalConfig):
+
+    TITLE: str = 'PROD'
+
+    HOST: str = "localhost"
+
+    DEBUG: bool = os.getenv("DEBUG")
+
+    BASE_PATH: str = 'path'
+
+    origins: str = '*'
+    PROJECT_NAME: str = "My Website"
+    PROJECT_VERSION: str = "1.0.0"
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DB_URL: str = os.getenv("DB_URL")
 
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
@@ -21,7 +96,32 @@ class Settings(BaseSettings):
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
     DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}/{POSTGRES_DB}"
 
+    openapi_url: Optional[str] = 'path'
+
+    docs_url: Optional[str] = 'path'
+
+    redoc_url: Optional[str] = None
+
+    proxy_headers: bool = True
+
+    forwarded_allow_ips: List[str] = ["*"]
+
+    WHITELISTED_IPS: List[str] = ["*"]
+
+
     model_config = SettingsConfigDict(case_sensitive=True)
 
 
-settings = Settings()
+class FactoryConfig:
+
+    def __init__(self, env_state: Optional[str]):
+        self.env_state = env_state
+
+    def __call__(self):
+        if self.env_state == "dev":
+            return DevConfig()
+
+        elif self.env_state == "prod":
+            return ProdConfig()
+
+
--- db/session.py
+++ db/session.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from .base_class import Base
 
-from config import settings
+def create_tables(settings):
+    SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+    print("Database URL is ", SQLALCHEMY_DATABASE_URL)
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-print("Database URL is ", SQLALCHEMY_DATABASE_URL)
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    Base.metadata.create_all(bind=engine)
